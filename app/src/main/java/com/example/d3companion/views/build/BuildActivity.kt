fee5ea493fe7af8589413a93fc4359dce0c1853f
Gregory Sholl e Santos <gregorysholl@gmail.com>
2019-02-21 15:11:03 +0100

    Fix gitignore

---
+++ app/src/main/java/com/example/d3companion/views/build/BuildActivity.kt
@@ -0,0 +1,13 @@
+package com.example.d3companion.views.build
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.d3companion.R
+
+class BuildActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_build)
+    }
+}
