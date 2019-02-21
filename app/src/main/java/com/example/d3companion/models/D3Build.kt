1107e889a92188f69cc20fae77dbc605546064c6
Gregory Sholl e Santos <gregorysholl@gmail.com>
2019-02-22 00:28:25 +0100

    Rename skill model for better consistency

--- app/src/main/java/com/example/d3companion/models/D3Build.kt
+++ app/src/main/java/com/example/d3companion/models/D3Build.kt
@@ -4,7 +4,9 @@ data class D3Build(
     val id: String,
     val name: String,
     val className: String,
+    val tier: String,
     val url: String,
+    val skills: D3SkillSet,
     val gear: List<D3Item>? = null,
     val gem: List<D3Item>? = null,
     val cube: List<D3Item>? = null
--- app/src/main/java/com/example/d3companion/models/D3SkillSet.kt
+++ app/src/main/java/com/example/d3companion/models/D3SkillSet.kt
@@ -1,6 +1,6 @@
 package com.example.d3companion.models
 
-data class D3Skills(
+data class D3SkillSet(
     val active: List<D3ActiveSkill>,
     val passive: List<D3Details>
 )
