557dabb13f9a1ba8e274de2e4e794216a43918ea
Gregory Sholl e Santos <gregorysholl@gmail.com>
2019-02-25 22:41:36 +0100

    Update models and new JSON reference for proper deserialization

--- app/src/main/java/com/example/d3companion/models/D3Button.kt
+++ app/src/main/java/com/example/d3companion/models/D3Button.kt
@@ -1,10 +1,12 @@
 package com.example.d3companion.models
 
+import com.google.gson.annotations.SerializedName
+
 enum class D3Button {
-    LeftMouse,
-    RightMouse,
-    FirstKey,
-    SecondKey,
-    ThirdKey,
-    FourthKey
+    @SerializedName("Left Mouse Button") LeftMouse,
+    @SerializedName("Right Mouse Button") RightMouse,
+    @SerializedName("1") FirstKey,
+    @SerializedName("2") SecondKey,
+    @SerializedName("3") ThirdKey,
+    @SerializedName("4") FourthKey
 }
--- app/src/main/java/com/example/d3companion/models/D3ItemDescription.kt
+++ app/src/main/java/com/example/d3companion/models/D3ItemDescription.kt
@@ -1,19 +1,28 @@
 package com.example.d3companion.models
 
+import com.google.gson.annotations.SerializedName
+
+//TODO: See if there is a more elegant way to express cube-related descriptions
 enum class D3ItemDescription {
-    Amulet,
-    Armor,
-    Belt,
-    Boots,
-    Bracers,
-    Chest,
-    Gem,
-    Gloves,
-    Helmet,
-    Jewelry,
-    Offhand,
-    Pants,
-    Ring,
-    Shoulders,
-    Weapon
+    //character-related
+    @SerializedName("amulet") Amulet,
+    @SerializedName("belt") Belt,
+    @SerializedName("boots") Boots,
+    @SerializedName("bracers") Bracers,
+    @SerializedName("chest") Chest,
+    @SerializedName("gloves") Gloves,
+    @SerializedName("helmet") Helmet,
+    @SerializedName("offhand") Offhand,
+    @SerializedName("pants") Pants,
+    @SerializedName("ring") Ring,
+    @SerializedName("shoulders") Shoulders,
+    @SerializedName("weapon") Weapon,
+
+    //gem-related
+    @SerializedName("gem") Gem,
+
+    //cube-related
+    @SerializedName("cube-armor") CubeArmor,
+    @SerializedName("cube-jewelry") CubeJewelry,
+    @SerializedName("cube-weapon") CubeWeapon
 }
